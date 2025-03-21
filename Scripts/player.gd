f89bb7e96e3d913850a68094d7c8e4a1f9d40121
Mochyk228 <muravskiy.bogdan@icloud.com>
2025-03-21 17:11:42 +0000

    21.05 Third

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -4,17 +4,37 @@ extends CharacterBody2D
 @export var gravity : float = 100.0
 @export var jump_velocity : float = 60.0
 @export var tile_map : TileMapLayer
+var mouse_pos : Vector2
+var tile_pos
+var old_tile_pos
+var is_the_same_place : bool
+var is_once : bool
 
 @onready var sprite: Sprite2D = $Sprite
 @onready var ray_cast: RayCast2D = $RayCast2D
+@onready var timer_animation: AnimatedSprite2D = $TimerAnimation
+@onready var timer: Timer = $Timer
 
 func _process(delta):
 	if Input.is_action_pressed("left_mouse_click"):
-		var pos = get_global_mouse_position()
-		var tile_pos = tile_map.local_to_map(pos)
-		var tile_data = tile_map.get_cell_tile_data(tile_pos)
-		tile_map.set_cell(tile_pos, -1)
-		print("I am colliding! at position: ", pos)
+		is_the_same_place = true
+		mouse_pos = get_global_mouse_position()
+		timer_animation.visible = true
+		timer_animation.play("default")
+		timer_animation.global_position = mouse_pos + Vector2(3,3)
+		tile_pos = tile_map.local_to_map(mouse_pos)
+		if tile_pos != old_tile_pos:
+			old_tile_pos = tile_pos
+			is_the_same_place = false 
+		
+		if is_the_same_place and not is_once:
+			timer.start()
+			is_once = true
+	else:
+		timer_animation.stop()
+		timer_animation.visible = false
+		timer.stop()
+		is_once = false
 
 func _physics_process(delta):
 	if not is_on_floor():
@@ -35,3 +55,7 @@ func _physics_process(delta):
 		else:
 			ray_cast.target_position.x = -10.0
 			sprite.flip_h = true
+
+
+func _on_timer_timeout() -> void:
+	tile_map.set_cell(tile_pos, -1)
